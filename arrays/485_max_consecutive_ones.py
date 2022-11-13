5d8e890246481d628de54a143e296bfd08b27ac7
Ethan S <105534679+esamnyu@users.noreply.github.com>
2022-11-13 14:58:35 -0500

    Rationale for why for i in nums didn't work

--- arrays/485_max_consecutive_ones.py
+++ arrays/485_max_consecutive_ones.py
@@ -6,6 +6,7 @@ class Solution:
         nums.append(0)
         # for i in range(0, len(nums)): -> start and stop 
         for i in range(len(nums)):
+            # Previously used for i in nums -? i is equal to the value at that index, so we're referencing index the value at index 0 or 1 which would always be 1
             # print(nums[i])
             if nums[i]==1:
                 count+=1
