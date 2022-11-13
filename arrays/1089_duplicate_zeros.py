2f53c16bbdef08fa6b965c607c80265b9b49d77e
esamnyu <105534679+esamnyu@users.noreply.github.com>
2022-11-13 17:08:58 -0500

    Solved duplicate zeros challenge
    Review this challenge to make sure you understand it

---
+++ arrays/1089_duplicate_zeros.py
@@ -0,0 +1,21 @@
+# Problem 1089 
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        hasDupe = False 
+        length_arr =len(arr)
+        
+        for i in range(len(arr)):
+            if arr[i] == 0:
+                if hasDupe:
+                    hasDupe = False
+                    continue
+                else:
+                    arr.insert(i, 0)
+                    arr.pop(length_arr)
+                    hasDupe = True
+                    
+       
+        
